75e6b52ca0fe7f679165988e9d0e19fd4aa1abb3
Halil Hakan Karabay <hakankarabay@icloud.com>
2023-08-08 23:54:28 +0300

    feat: added some properties and relationships to task

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -8,13 +8,56 @@ use App\Filters\Helper\Filterable;
 use App\Filters\TaskFilter;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Task extends Model implements IFilterable
 {
     use HasFactory, Filterable;
 
+    protected $with = [
+        'category',
+        'assigned',
+        'createdBy',
+    ];
+
+    protected $fillable = [
+        'created_by',
+        'assigned_to',
+        'title',
+        'description',
+        'progress',
+        'category_id',
+    ];
+
+    /**
+     * @return BaseFilter
+     */
     public function getFilterClass(): BaseFilter
     {
         return new TaskFilter;
+    }
+
+    /**
+     * @return BelongsTo
+     */
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(TaskCategory::class, 'category_id');
+    }
+
+    /**
+     * @return BelongsTo
+     */
+    public function assigned(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'assigned_to');
+    }
+
+    /**
+     * @return BelongsTo
+     */
+    public function createdBy(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'created_by');
     }
 }
