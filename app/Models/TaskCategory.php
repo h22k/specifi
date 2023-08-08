40d653edb22ead1620899837529d1ce8e7c7103a
Halil Hakan Karabay <hakankarabay@icloud.com>
2023-08-09 00:01:48 +0300

    feat: added relationships to task category

--- app/Models/TaskCategory.php
+++ app/Models/TaskCategory.php
@@ -4,8 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class TaskCategory extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name'
+    ];
+
+    /**
+     * @return HasMany
+     */
+    public function tasks(): HasMany
+    {
+        return $this->hasMany(Task::class, 'category_id');
+    }
 }
