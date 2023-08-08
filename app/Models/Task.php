0a3d7bb531a7ef0638f0ef03563859936387ad57
Halil Hakan Karabay <hakankarabay@icloud.com>
2023-08-08 22:01:59 +0300

    feat: added filterable trait and interface to task model

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -2,10 +2,19 @@
 
 namespace App\Models;
 
+use App\Filters\BaseFilter;
+use App\Filters\Contracts\IFilterable;
+use App\Filters\Helper\Filterable;
+use App\Filters\TaskFilter;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Task extends Model
+class Task extends Model implements IFilterable
 {
-    use HasFactory;
+    use HasFactory, Filterable;
+
+    public function getFilterClass(): BaseFilter
+    {
+        return new TaskFilter;
+    }
 }
